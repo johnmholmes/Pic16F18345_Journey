324f38f983eea38ce3a67dff1c6e89eaf55ef152
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2023-03-24 14:31:30 +0000

    Code added

---
+++ Blibk v 1/Blink_v1_18345.c
@@ -0,0 +1,64 @@
+/*
+ * File:   main.c
+ * Author: John Holmes
+ * Target: PIC16F18345
+ * Compiler: XC8 v2.36
+ * IDE: MPLABX v6.00
+ *
+ * Created on March 23, 2023, 12:22 PM
+ */
+
+// Define the configuration bits
+#pragma config FEXTOSC = OFF    // FEXTOSC External Oscillator mode Selection bits (Oscillator not enabled)
+#pragma config RSTOSC = HFINT32 // Power-up default value for COSC bits (HFINTOSC with 2x PLL (32MHz))
+#pragma config CLKOUTEN = OFF   // Clock Out Enable bit (CLKOUT function is disabled; I/O or oscillator function on OSC2)
+#pragma config CSWEN = OFF      // Clock Switch Enable bit (The NOSC and NDIV bits cannot be changed by user software)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
+
+// CONFIG2
+#pragma config MCLRE = OFF      // Master Clear Enable bit (MCLR/VPP pin function is digital input; MCLR internally disabled; Weak pull-up under control of port pin's WPU control bit.)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bits (WDT disabled; SWDTEN is ignored)
+#pragma config LPBOREN = OFF    // Low-power BOR enable bit (ULPBOR disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bits (Brown-out Reset disabled)
+#pragma config BORV = LOW       // Brown-out Reset Voltage selection bit (Brown-out voltage (Vbor) set to 2.45V)
+#pragma config PPS1WAY = ON     // PPSLOCK bit One-Way Set Enable bit (The PPSLOCK bit can be cleared and set only once; PPS registers remain locked after one clear/set cycle)
+#pragma config STVREN = OFF     // Stack Overflow/Underflow Reset Enable bit (Stack Overflow or Underflow will not cause a Reset)
+#pragma config DEBUG = OFF      // Debugger enable bit (Background debugger disabled)
+
+// CONFIG3
+#pragma config WRT = OFF        // User NVM self-write protection bits (Write protection off)
+#pragma config LVP = ON         // Low Voltage Programming Enable bit (Low Voltage programming enabled. MCLR/VPP pin function is MCLR. MCLRE configuration bit is ignored.)
+
+// CONFIG4
+#pragma config CP = OFF         // User NVM Program Memory Code Protection bit (User NVM code protection disabled)
+#pragma config CPD = OFF        // Data NVM Memory Code Protection bit (Data NVM code protection disabled)
+
+// Include the necessary header files
+#include <xc.h>
+
+// Define the oscillator frequency (in Hz)
+#define _XTAL_FREQ 32000000
+
+// Main program
+void main(void)
+{
+    // Configure RC0 as an output pin
+    TRISBbits.TRISB6 = 0;
+    
+    // Loop forever
+    while(1)
+    {
+        // Turn on the LED
+        LATBbits.LATB6 = 1;
+        
+        // Delay for a short period
+        __delay_ms(100);
+        
+        // Turn off the LED
+        LATBbits.LATB6 = 0;
+        
+        // Delay for a short period
+        __delay_ms(100);
+    }
+}
